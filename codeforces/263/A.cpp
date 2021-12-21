504e4ac604a1770b3bbc5aadba16bb7ce39a2925
OsamaMaani <thephilologueprog@gmail.com>
2021-12-21 17:01:00 +0200

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/140177456

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -6,23 +6,30 @@ typedef long long ll;
 
 int main()
 {
+
    int arr[5][5];
+   int sum =0;
    int row_index = 0;
-   int col_index = 0;
-   int steps = 0;
-   for(int i = 0 ; i <5; i++){
-    for(int j = 0 ; j<5; j++){
+   int column_index=0;
+   for(int i =0 ; i<5; i++){
+    for(int  j = 0; j<5; j++){
         cin>>arr[i][j];
-        if(arr[i][j] == 1){
-            row_index = i;
-            col_index = j;
-        }
     }
    }
-   steps += abs(row_index -2);
-   steps += abs(col_index -2);
 
-   cout<<steps<<endl;
+   for(int i = 0 ; i<5; i++){
+      for(int j = 0; j<5; j++){
+         if(arr[i][j] == 1){
+            row_index = i;
+            column_index = j;
+         }
+      }
+   }
+
+   sum += abs(column_index - 2);
+   sum += abs(row_index-2);
+
+   cout<<sum;
 
 	return 0;
 }
